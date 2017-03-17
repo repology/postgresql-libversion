979f7c62d52588ae9b305bc32f5926a3ad2ecca9
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-03-17 20:05:28 +0300

    Add regression test

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 EXTENSION = libversion
 DATA = libversion--1.0.0.sql
 MODULE_big = libversion
+REGRESS = libversion_test
 
 PG_CPPFLAGS = `pkg-config --cflags libversion` -I bar
 SHLIB_LINK += `pkg-config --libs libversion` -L foo
---
+++ sql/libversion_test.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION libversion;
+
+SELECT version_compare_simple('0.1', '1.0');
+SELECT version_compare_simple('1.0', '0.1');
+SELECT version_compare_simple('1.0', '1.0');
+
+SELECT version_compare_simple('1.0', '1.0.0');
+SELECT version_compare_simple('1.0', '1.0a');
+SELECT version_compare_simple('1.0', '1..0');
