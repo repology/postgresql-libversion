570bc04b5c2fda7a108fad00ab18258ffcf89067
Dmitry Marakasov <amdmi3@amdmi3.ru>
2018-10-29 21:53:51 +0300

    Update tests

--- sql/libversion.sql
+++ sql/libversion.sql
@@ -4,14 +4,32 @@ CREATE EXTENSION libversion;
 -- Test standalone functions
 --
 
--- version_compare_simple
+-- version_compare_simple (deprecated)
 SELECT version_compare_simple('0.1', '1.0') AS t;
-SELECT version_compare_simple('1.0', '0.1') AS t;
-SELECT version_compare_simple('1.0', '1.0') AS t;
 
-SELECT version_compare_simple('1.0', '1.0.0') AS t;
-SELECT version_compare_simple('1.0', '1.0a') AS t;
-SELECT version_compare_simple('1.0', '1..0') AS t;
+-- version_compare2
+SELECT version_compare2('0.1', '1.0') AS t;
+SELECT version_compare2('1.0', '0.1') AS t;
+SELECT version_compare2('1.0', '1.0') AS t;
+
+SELECT version_compare2('1.0', '1.0.0') AS t;
+SELECT version_compare2('1.0', '1.0a') AS t;
+SELECT version_compare2('1.0', '1..0') AS t;
+
+-- flags
+SELECT VERSIONFLAG_P_IS_PATCH();
+SELECT VERSIONFLAG_ANY_IS_PATCH();
+
+-- version_compare4
+SELECT version_compare4('1.0p1', '1.0p1', 0, 0) AS t;
+SELECT version_compare4('1.0p1', '1.0p1', 0, VERSIONFLAG_P_IS_PATCH()) AS t;
+SELECT version_compare4('1.0p1', '1.0p1', VERSIONFLAG_P_IS_PATCH(), 0) AS t;
+SELECT version_compare4('1.0p1', '1.0p1', VERSIONFLAG_P_IS_PATCH(), VERSIONFLAG_P_IS_PATCH()) AS t;
+
+SELECT version_compare4('1.0h1', '1.0h1', 0, 0) AS t;
+SELECT version_compare4('1.0h1', '1.0h1', 0, VERSIONFLAG_ANY_IS_PATCH()) AS t;
+SELECT version_compare4('1.0h1', '1.0h1', VERSIONFLAG_ANY_IS_PATCH(), 0) AS t;
+SELECT version_compare4('1.0h1', '1.0h1', VERSIONFLAG_ANY_IS_PATCH(), VERSIONFLAG_ANY_IS_PATCH()) AS t;
 
 --
 -- Test type
@@ -66,12 +84,15 @@ SELECT MAX(ver) AS "1.100" FROM srt;
 set enable_seqscan = off;
 SELECT ver FROM srt ORDER BY ver;
 reset enable_seqscan;
+
 set enable_indexscan = off;
 SELECT ver FROM srt ORDER BY ver;
 reset enable_indexscan;
+
 set enable_seqscan = off;
 SELECT ver FROM srt ORDER BY ver DESC;
 reset enable_seqscan;
+
 set enable_indexscan = off;
 SELECT ver FROM srt ORDER BY ver DESC;
 reset enable_indexscan;
